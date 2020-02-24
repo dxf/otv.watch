1994c261cb75b17f23032a6a59c6aae98d885211
Dafydd <me@daf.lol>
2020-02-24 13:25:43 +0000

    broke, fix maybe

--- live.py
+++ live.py
@@ -5,4 +5,4 @@ def header():
     import getstatus
     return render_template('live.html',monstercat=getstatus.monstercat,poki=getstatus.poki,scarra=getstatus.scarra,lily=getstatus.lily,fed=getstatus.fed,yvonne=getstatus.yvonne)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run()
