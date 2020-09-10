ff0bec55bc3c0cbca755f0d216147162092a5853
Dafydd <daf@daf.lol>
2020-09-10 11:16:44 +0100

    fix

--- getstatus.py
+++ getstatus.py
@@ -1,7 +1,66 @@
-import twitch
-helix = twitch.Helix('lr8ok7604ketqe6qq5kq1bdieibcpu')
-poki = bool(helix.user('pokimane').is_live)
-scarra = bool(helix.user('scarra').is_live)
-lily = bool(helix.user('lilypichu').is_live)
-yvonne = bool(helix.user('yvonnie').is_live)
-monstercat = bool(helix.user('monstercat').is_live)
+import requests
+headers = {
+    'Authorization': 'Bearer txsm06jq4bzk6ikd7fnx1rtqmns2qz',
+    'Client-ID': 'lr8ok7604ketqe6qq5kq1bdieibcpu',
+}
+
+params = (
+('user_login', 'pokimane'),
+)
+response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=params)
+j = response.json()
+if len(str(j)) < 100:
+    poki = False
+else:
+    poki = True
+print(j)
+
+params = (
+('user_login', 'scarra'),
+)
+response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=params)
+j = response.json()
+if len(str(j)) < 100:     
+    scarra = False
+else:
+    scarra = True
+
+params = (
+('user_login', 'lilypichu'),
+)
+response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=params)
+j = response.json()
+if len(str(j)) < 100:
+    lily = False
+else:
+    lily = True
+
+params = (
+('user_login', 'yvonnie'),
+)
+response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=params)
+j = response.json()
+if len(str(j)) < 100:
+    yvonne = False
+else:
+    yvonne = True
+
+params = (
+('user_login', 'monstercat'),
+)
+response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=params)
+j = response.json()
+if len(str(j)) < 100:
+    monstercat = False
+else:
+    monstercat = True
+
+params = (
+('user_login', 'michaelreeves'),
+)
+response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=params)
+j = response.json()
+if len(str(j)) < 100:
+    mykull = False
+else:
+    mykull = True
