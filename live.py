e8a2bc2ae7f863ac02da5cca5d5f626ffcb7bdff
Dafydd <me@daf.lol>
2020-09-10 09:01:53 +0100

    Update live.py

--- live.py
+++ live.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 @app.route('/')
 def header():
     import getstatus
-    return render_template('live.html',monstercat=getstatus.monstercat,poki=getstatus.poki,scarra=getstatus.scarra,lily=getstatus.lily,fed=getstatus.fed,yvonne=getstatus.yvonne)
+    return render_template('live.html',monstercat=getstatus.monstercat,poki=getstatus.poki,scarra=getstatus.scarra,lily=getstatus.lily,yvonne=getstatus.yvonne)
 if __name__ == '__main__':
     app.run()
